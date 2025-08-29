2a46e10b2e9d5a3d0b85da6f310998b635cb508b
terddyy <terddy03@gmail.com>
2025-08-29 04:13:59 -0400

    log in

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -1,18 +1,16 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
+from django.views.generic import TemplateView
+
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    # path for login API
+    path(route='login', view=views.login_user, name='login'),
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    # path for login page (React)
+    path('login/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
